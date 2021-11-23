96764cd1e498f755744f84551ff54f0758607dbf
Amirul <amirulcse@gmail.com>
2021-11-23 20:16:29 +0100

    This program is used to find out min number!!

---
+++ MminNumberCheck.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter value for number1:"))
+num2 = int(input("Enter value for number2:"))
+num3 = int(input("Enter value for number3:"))
+
+if num1<num2 and num1<num3:
+    print("The large number is num1:", num1)
+
+elif num2<num1 and num2<num3:
+    print("The large number is num2:", num2)
+
+else:
+    print("The large number is num3:", num3)
