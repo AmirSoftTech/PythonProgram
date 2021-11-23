84bc9d90e0ba4f43263d071318d67e04d7fd30ef
Amirul <amirulcse@gmail.com>
2021-11-23 22:33:57 +0100

    While loop break and continue!!

---
+++ Break_Continue.py
@@ -0,0 +1,21 @@
+#Break
+input= int(input("Enter value:"))
+i = 1
+
+while i<=input:
+    print(i)
+    i = i +1
+    if i==10:
+        break
+#--------------------------------------------------------------
+#Continue
+'''
+input= int(input("Enter value:"))
+i = 1
+
+while i<=input:
+    print(i)
+    i = i +1
+    if i==10:
+        continue
+'''
