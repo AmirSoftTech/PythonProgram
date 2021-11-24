f9d112bbb20d3317aa60bd5a0a91b84c6753cd0a
Amirul <amirulcse@gmail.com>
2021-11-24 12:14:49 +0100

    For loop with series 1 + 2 + 3 + ....+ n #1 + 2+ 3 + 4 + 5 +.............+ n #2 + 4 + 6 + 8 + ........+ n # 1*1 + 2*2 + 3*3 + ........+ n*n # 2*2 + 4*4 + 6*6 + ........+ n*n

--- Series.py
+++ Series.py
@@ -1,7 +1,48 @@
 #1 + 2+ 3 + 4 + 5 +.............+ n
+'''
 n = int(input("Enter value:"))
 sum = 0
 
 for x in range(1, n+1, 1):
     sum = sum + x
+print(sum)
+'''
+
+#2 + 4 + 6 + 8 + ........+ n
+'''
+n = int(input("Enter value:"))
+sum = 0
+
+for x in range(2, n+1, 2):
+    sum = sum + x
+print(sum)
+
+'''
+
+# 1*1 + 2*2 + 3*3 + ........+ n*n
+'''
+n = int(input("Enter value:"))
+sum = 0
+
+for x in range(1, n+1, 1):
+    sum = sum + x*x
+print(sum)
+
+'''
+
+# 2*2 + 4*4 + 6*6 + ........+ n*n
+'''
+n = int(input("Enter value:"))
+sum = 0
+
+for x in range(2, n+1, 2):
+    sum = sum + x*x
+print(sum)
+
+'''
+n = int(input("Enter value:"))
+sum = 1
+
+for x in range(1, n+1, 1):
+    sum = sum * x
 print(sum)
