b43f0689319a0f559a3fbc7c265c0f5be607a505
Amirul <amirulcse@gmail.com>
2021-11-25 16:57:16 +0100

    Split of list data

--- ListString.py
+++ ListString.py
@@ -1,5 +1,5 @@
-n = input("Enter value:")# 10 20 30 40 50 60
-list = n.split()# 10, 20, 30, 40, 50, 60
+n = input("Enter value:")# 10 20 30 40 50
+list = n.split()# 10, 20, 30, 40, 50
 print(list)
 
 sum = 0
