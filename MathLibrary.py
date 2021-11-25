f6bcd00ba2f2cbc80c0c6564d77b3227f73751cf
Amirul <amirulcse@gmail.com>
2021-11-25 12:46:24 +0100

    Math Related Library function!!

---
+++ MathLibrary.py
@@ -0,0 +1,19 @@
+from math import *
+
+max =max(10,20,30,80,40,60)
+min = min(10,20,30,80,40,60)
+abs = abs(-4)
+power = pow(2,3)
+sqrt = sqrt(25)
+round = round(3.6)
+floor = floor(5.5)
+ceil = ceil(5.5)
+
+print("Max number is:", max)
+print("Min number is:", min)
+print("Absolute number is:", abs)
+print("Power number is:", power)
+print("Square root number is:", sqrt)
+print("Round number is:", round)
+print("Floor number is : ",floor)
+print("Ceil number is : ",ceil)
