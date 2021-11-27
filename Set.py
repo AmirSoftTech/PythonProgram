8ec786ae80fc80ea82889619d887cccbb65278f4
Amirul <amirulcse@gmail.com>
2021-11-27 14:20:08 +0100

    Sets are used to store multiple items in a single variable

--- Set.py
+++ Set.py
@@ -1,5 +1,10 @@
 num1 ={1, 2, 3, 4, 5}
 num2 = set([4, 5, 6, 7, 8, 9, 10])
 
-print(num1 | num2)
-print(num1 & num2)
+num1.add(6)#Add new data 6
+print(num1)
+num1.remove(6)#Remove new data 6
+print(num1)
+print(num1 | num2)#union set
+print(num1 & num2)#Intersection set
+print(num1-num2)
