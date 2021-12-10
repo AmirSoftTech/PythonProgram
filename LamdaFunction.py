cbdc3d397ddd1706dc9ca5c292a4eb03128447dd
Amirul <amirulcse@gmail.com>
2021-12-10 12:34:05 +0100

    Example for lamda function

---
+++ LamdaFunction.py
@@ -0,0 +1,14 @@
+#Example for lamda function
+x = (lambda a, b: a * a + 2 * a * b + b * b)(2,2)
+print(x)
+
+y = (lambda x: x * x * x)(2)
+print(y)
+
+#Normal function returns value
+'''
+def fun (c):
+    return c * c *c
+cc = fun(3)
+print(cc)
+'''
