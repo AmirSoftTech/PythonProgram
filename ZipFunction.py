e6cb664e5eec9acaf846ff25be27dfd06794dcc0
Amirul <amirulcse@gmail.com>
2021-12-11 15:39:13 +0100

    Zip function example!!

--- ZipFunction.py
+++ ZipFunction.py
@@ -1,18 +1,22 @@
 id = [101, 102, 103, 104, 105, 106]
 name = ["Amirul Islam","Tania Islam", "Arian Islam", "Waliul Islam", "Tahmina Islam", "Tushar Islam"]
+address = ["America", "USA", "Asia", "Japan", "Canada", "Australia"]
 
-x = list(zip(id, name))
+# id, name and address
+x = list(zip(id, name, address))
 for i in x:
     print(i)
 
 print("-----------------------------------------------------------------------")
 
+# id and name
 y = list(zip("123456", id, name))
 for j in y:
     print(j)
 
 print("-----------------------------------------------------------------------")
 
-z = list(zip( "ABCDEF", id, name))
+# id and name
+z = list(zip( "ABCDEF", id, name)) # id and name
 for k in z:
     print(k)
