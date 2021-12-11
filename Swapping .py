14d835c2c607dce0136705bd0b67c60ed2e8b323
Amirul <amirulcse@gmail.com>
2021-12-11 23:26:04 +0100

    Swapping value between number two!

---
+++ Swapping .py
@@ -0,0 +1,24 @@
+a = 10
+b = 20
+print("Before swapping :")
+print("a:",a, "\nb:",b)
+
+print("After swapping :")
+c = 0
+c = a
+a = b
+b = c
+print("a:",a, "\nb:",b)
+
+#Alter native swapping approach
+'''
+a = 10
+b = 20
+print("Before swapping :")
+print("a:",a, "\nb:",b)
+
+print("After swapping :")
+a, b = b,a
+print("a:",a, "\nb:",b)
+'''
+
