2c378fcf16000af8370f5e5ef534504da4174928
Amirul <amirulcse@gmail.com>
2021-12-13 21:26:55 +0100

    OOP: class and object!

---
+++ OOP/ClassObject.py
@@ -0,0 +1,17 @@
+class Student:
+    roll = ""
+    gpa = ""
+
+rahim = Student()
+x = rahim.roll = 10
+y = rahim.gpa = 3.75
+print("Rahim's roll :",x)
+print("Rahim's Gpa :",y)
+
+print("---------------------------------------------------------")
+
+karim = Student()
+x = karim.roll = 20
+y = karim.gpa = 3.80
+print("karim's roll :",x)
+print("karim's Gpa :",y)
